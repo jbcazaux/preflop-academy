138f1e0c1e54eb5b91c57e625cf689a1558d3c26
jbcazaux <jbcazaux@gmail.com>
2022-12-24 19:51:33 +0100

    lint

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -1,9 +1,10 @@
 import { PropsWithChildren } from 'react'
 
-interface Props extends PropsWithChildren<any> {
+interface Props extends PropsWithChildren<unknown> {
   title: string
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Tab = (_: Props) => null
 
 export default Tab
